7be61daa1cf3e05da6118e85b8da46ee85f5b42d
Tammy Fina <finatammy@gmail.com>
2022-03-17 11:37:21 -0400

    herou challenges

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });

c0f2f20a188547f5be020ce5113ee49f2a79e58c
Tammy Fina <finatammy@gmail.com>
2022-03-28 08:51:35 -0400

    webpack and service worker

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://0.0.0.0/budget-tracker";
 
 const app = express();
 
@@ -18,7 +18,8 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true 
 });
 
 // routes

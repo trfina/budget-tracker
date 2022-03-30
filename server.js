eaf8005881ea41b6612b8a1e584d80f67f489815
Tammy Fina <finatammy@gmail.com>
2022-03-30 13:34:58 -0400

    mongoDB update

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+// trying this code to get mongoDB to work
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://trfina:<PwMongo001#>@cluster0.e3lwb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+// end test code
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://0.0.0.0/budget-tracker";
 
